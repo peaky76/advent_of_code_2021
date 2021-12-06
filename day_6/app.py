6643eead1105632f4c1f1457d788504fa67c8878
peaky76 <robertjamespeacock@gmail.com>
2021-12-06 22:07:24 +0000

    Day 6 condensed

--- day_6/app.py
+++ day_6/app.py
@@ -1,27 +1,13 @@
 from collections import Counter
-import time
+
+fish = [int(item) - 8 for line in open("puzzle_input.txt", "r") for item in line.split(',')]
 
 def get_count(initial_fish, num_of_days):
     count_dict = Counter(initial_fish)
-
-    for i in range(-8, num_of_days + 1):
-        spawning_days = [x for x in range(i + 9, num_of_days + 1, 7)] 
-        for day in spawning_days:
-            count_dict[day] += count_dict[i]
-    
+    for day_in_cycle in range(-8, num_of_days + 1):
+        for spawning_day in range(day_in_cycle + 9, num_of_days + 1, 7):
+            count_dict[spawning_day] += count_dict[day_in_cycle]
     return sum(count_dict.values())
-                              
-file = open("puzzle_input.txt", "r")
-fish = [int(item) - 8 for line in file for item in line.split(',')]
 
-# PART ONE
-bef = time.perf_counter()
-print(get_count(fish, 80))    
-aft = time.perf_counter()
-print(aft - bef)
-
-# PART TWO
-bef = time.perf_counter()
-print(get_count(fish, 256))    
-aft = time.perf_counter()
-print(aft - bef)
+print(get_count(fish, 80)) # PART ONE
+print(get_count(fish, 256)) # PART TWO
